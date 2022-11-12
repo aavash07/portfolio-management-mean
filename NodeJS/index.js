2ff3e487e232e5755a00d083285f0853fb349811
Aavash Chaudhary <aavashchaudhary@bajratechnologies.com>
2022-11-12 23:13:45 +0545

    feat: route for not matching routes

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -16,3 +16,15 @@ app.listen(3000, () => console.log('Server listening on port: 3000'));
 app.use('/stocks', stockController);
 app.use('/stock_details', stockDetailController);
 app.use('/users', userController);
+
+// This should be the last route else any after it won't work
+app.use('*', (req, res) => {
+    res.status(404).json({
+      success: 'false',
+      message: 'Page not found',
+      error: {
+        statusCode: 404,
+        message: 'You reached a route that is not defined on this server',
+      },
+    });
+  });
